20593e5534f9002a36e9d5e0dd1cab52ae19f76e
Shane Smyth <shanesmyth8@gmail.com>
2018-06-25 22:44:31 -0500

    Update

--- server.js
+++ server.js
@@ -30,14 +30,6 @@ if ('development' == app.get('env')) {
 
 // HubExchange Routes
 app.get('/', routes.index );
-app.post('/login', routes.login );
-app.post('/logout', routes.logout );
-
-// Custom Hello World Activity Routes
-app.post('/save/', activity.save );
-app.post('/validate/', activity.validate );
-app.post('/publish/', activity.publish );
-app.post('/execute/', activity.execute );
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
