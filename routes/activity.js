92330a61e4c0d87b620c98cf3011612825cfd22b
Shane Smyth <shanesmyth8@gmail.com>
2018-06-25 23:08:11 -0500

    Update

--- routes/activity.js
+++ routes/activity.js
@@ -3,7 +3,6 @@ var util = require('util');
 
 // Deps
 const Path = require('path');
-const JWT = require(Path.join(__dirname, '..', 'lib', 'jwtDecoder.js'));
 var util = require('util');
 var http = require('https');
 var request = require('request');
