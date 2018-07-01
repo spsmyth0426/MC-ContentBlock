23ca84b05a5e2f2342ce26d679aa29b3cf724a8c
Shane Smyth <shanesmyth8@gmail.com>
2018-06-30 19:56:39 -0500

    Update

--- server.js
+++ server.js
@@ -33,69 +33,68 @@ if ('development' == app.get('env')) {
 app.get('/', function(request, response, next) {
     if (window.self === window.top) {
       document.body.innerText = 'This application is for use in the Salesforce Marketing Cloud Content Builder Editor only.';
-  } else {
-      var sdk = new BlockSDK();
-      sdk.getContent(function (content) {
-          //quill.root.innerHTML = content;
-          function saveText() {
-              /********Setup Sync*******/
-              const options = {
-                  uri: '/data/v1/async/dataextensions/key:'+platformDE+'/rows',
-                  headers: {},
-                  body: {"items": [{
-                      "Id":"1",
-                      "Name":"Category",
-                      "Value" : "BarCode",
-                      "EmailId": "23456"
-                  }]}
-                  // other request options
-              };
-      
+    } else {
+        var sdk = new BlockSDK();
+        sdk.getContent(function (content) {
+            //quill.root.innerHTML = content;
+            function saveText() {
+                /********Setup Sync*******/
+                const options = {
+                    uri: '/data/v1/async/dataextensions/key:'+platformDE+'/rows',
+                    headers: {},
+                    body: {"items": [{
+                        "Id":"1",
+                        "Name":"Category",
+                        "Value" : "BarCode",
+                        "EmailId": "23456"
+                    }]}
+                    // other request options
+                };
+        
 
-              /********Set Content*******/
-              //var html = quill.root.innerHTML;
-              //sdk.setContent(html);
-              RestClient.put(options)
-              .then(response => {
-                  // will be delivered with 200, 400, 401, 500, etc status codes
-                  // response.body === payload from response
-                  // response.res === full response from request client
-                  console.log(response);
-              })
-              .catch(err => console.log(err));
-              //sdk.setContent('Set @name = "Barcode"');
-              sdk.setSuperContent('This is super content: ' + html);
+                /********Set Content*******/
+                //var html = quill.root.innerHTML;
+                //sdk.setContent(html);
+                RestClient.put(options)
+                .then(response => {
+                    // will be delivered with 200, 400, 401, 500, etc status codes
+                    // response.body === payload from response
+                    // response.res === full response from request client
+                    console.log(response);
+                })
+                .catch(err => console.log(err));
+                //sdk.setContent('Set @name = "Barcode"');
+                sdk.setSuperContent('This is super content: ' + html);
 
-              /********Get Content*******/
-              sdk.getData(function (data) {
-                  var numberOfEdits = data.numberOfEdits || 0;
-                  sdk.setData({
-                      numberOfEdits: numberOfEdits + 1
-                  });
-              });
-              sdk.getCentralData(function (central) {
-                  var totalNumberOfEdits = central.totalNumberOfEdits || 0;
-                  sdk.setCentralData({
-                      totalNumberOfEdits: totalNumberOfEdits + 1
-                  });
-              });
-          }
-          
-          playground();
-          sdk.setSuperContent('This is super content: ' + html);
-          //quill.on('text-change', saveText);
-          console.log('Inside getContent');
-          
-          res.render( 'index', {
-              title: 'MC CB Custom Attributes',
-              results: activity.logExecuteData,
-          });
-      });
+                /********Get Content*******/
+                sdk.getData(function (data) {
+                    var numberOfEdits = data.numberOfEdits || 0;
+                    sdk.setData({
+                        numberOfEdits: numberOfEdits + 1
+                    });
+                });
+                sdk.getCentralData(function (central) {
+                    var totalNumberOfEdits = central.totalNumberOfEdits || 0;
+                    sdk.setCentralData({
+                        totalNumberOfEdits: totalNumberOfEdits + 1
+                    });
+                });
+            }
+            
+            playground();
+            sdk.setSuperContent('This is super content: ' + html);
+            //quill.on('text-change', saveText);
+            console.log('Inside getContent');
+            
+            res.render( 'index', {
+                title: 'MC CB Custom Attributes',
+                results: activity.logExecuteData,
+            });
+        });
 
-  }
+    }
   logData(req);
-  };
-});
+  });
 
 app.get('/blockSave', routes.blockSave );
 
