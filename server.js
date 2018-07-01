67446a76211b6085cb5cb3739cefa84b5520369b
Shane Smyth <shanesmyth8@gmail.com>
2018-06-30 20:04:04 -0500

    Update

--- server.js
+++ server.js
@@ -14,7 +14,6 @@ var app = express();
 
 // Configure Express
 app.set('port', process.env.PORT || 3000);
-app.use(bodyParser.raw({type: 'application/jwt'}));
 app.use(bodyParser.json());
 //app.use(bodyParser.urlencoded({ extended: true }));
 
