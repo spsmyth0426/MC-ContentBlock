58c6444142bc117ed8c13f1c42e80d6cfdcf0701
Shane Smyth <shanesmyth@Shanes-MBP.hsd1.mn.comcast.net>
2018-07-24 20:12:04 -0500

    update

--- routes/sports.js
+++ routes/sports.js
@@ -7,7 +7,7 @@ var util = require('util');
 
 var MySportsFeeds = require("mysportsfeeds-node");
 var msf = new MySportsFeeds("1.2", true,  null);
-msf.authenticate("spsmyth0426", "kf45wxB6");
+msf.authenticate("c2791441-c7ce-492e-b5b4-d47ce8", "kf45wxB6");
 
 
 /*
@@ -19,7 +19,34 @@ exports.sportsEndPoint = function(req, res){
     var values = req.body.values;
     console.log(deExternalKey);*/
 
-    var data = msf.getData('nba', '2016-2017-regular', 'player_gamelogs', 'json', {player: 'stephen-curry'});
+    var data = msf.getData('nba', '2017-2018-regular', 'player_gamelogs', 'json', {player: 'stephen-curry'});
+
+    //pullSports('nba', '2016-2017-regular', 'player_gamelogs', 'json', "{player: 'stephen-curry'}");
 
     console.log('NBA: '+JSON.stringify(data, null, 2));
 };
+
+function pullSports(league, season, route, format, params){
+    var optionsDE = {
+        url: 'https://api.mysportsfeeds.com/v2.0/pull/'+league+'/'+season+'/'+route+'.'+format,
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer '+accessToken
+        },
+        //form: {'values': {'ContactId': contact, 'Status': 'Confirmed', 'ResponseId': responseId}}
+        body: [ { "items": inArgs } ],
+        json: true
+    }
+    console.log(optionsDE);
+
+    request(optionsDE, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            // Print out the response body
+            console.log('Post to DE successful');
+        }else{
+            console.log('Post to DE: error');
+            console.log(body);
+        }
+    })
+}
